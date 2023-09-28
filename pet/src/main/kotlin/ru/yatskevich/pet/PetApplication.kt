54a1cc00cd8c82a1be7250d27eb672f7053b64f7
a.yatskevich <a.yatskevich@tinkoff.ru>
2023-09-28 16:20:07 +0500

    init empty app

---
+++ pet/src/main/kotlin/ru/yatskevich/pet/PetApplication.kt
@@ -0,0 +1,11 @@
+package ru.yatskevich.pet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetApplication>(*args)
+}
